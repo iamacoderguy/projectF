b84acf187d4603ecc4d70b24c1cf9bc503ecb14d
IM Coder <iamacoderguy@gmail.com>
2020-12-23 08:43:18 +0700

    Remove TODOs

--- movie-watchlist/src/App.tsx
+++ movie-watchlist/src/App.tsx
@@ -6,10 +6,6 @@ import Watched from "./components/Watched";
 import Add from "./components/Add";
 import { GlobalProvider } from "./context/GlobalState";
 
-// TODO https://medium.com/better-programming/how-to-deploy-your-react-app-to-heroku-aedc28b218ae
-// heroku git:remote -a az-projecta
-// git subtree push --prefix path-to-sub-direction heroku master
-
 function App() {
   return (
     <GlobalProvider>
