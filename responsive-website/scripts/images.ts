256fbf0f7fb668e1feacc171ce8a3eff99202e3f
IM Coder <iamacoderguy@gmail.com>
2020-12-26 16:54:03 +0700

    Images

---
+++ responsive-website/scripts/images.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import fsRecursive from 'fs-readdir-recursive';
+
+const imagesPath = 'src/images';
+
+const getUniqueImageFiles = () => {
+  const array = fsRecursive(imagesPath)
+    .filter((file) => {
+      return file.endsWith('.png') || file.endsWith('.svg') || file.endsWith('.jpg');
+    })
+    .map((file) => {
+      return file
+        .replace('@2x.png', '.png')
+        .replace('@3x.png', '.png');
+    });
+
+  return Array.from(new Set(array));
+};
+
+const generate = () => {
+  const properties = getUniqueImageFiles()
+    .map((file) => {
+      const name = file
+        .substring(0, file.lastIndexOf('.'))
+        .replace(/-/g, '_')
+        .replace(/\\/g, '_')
+        .replace(/ /g, '_');
+      const path = file
+        .replace(/\\/g, '/');
+      return `${name}: require('./${path}')`;
+    })
+    .join(',\n  ');
+
+  const string = `const images = {
+  ${properties},
+};
+export default images;
+`;
+
+  fs.writeFileSync(imagesPath + '/index.ts', string.replace(/\n/g, '\r\n'), 'utf8');
+};
+
+generate();
---
+++ responsive-website/src/images/index.ts
@@ -0,0 +1,18 @@
+const images = {
+  cli: require('./cli.png'),
+  cloud: require('./cloud.png'),
+  docs: require('./docs.png'),
+  favicon: require('./favicon.png'),
+  logos_clojure: require('./logos/clojure.png'),
+  logos_csharp: require('./logos/csharp.png'),
+  logos_go: require('./logos/go.png'),
+  logos_node: require('./logos/node.png'),
+  logos_php: require('./logos/php.png'),
+  logos_python: require('./logos/python.png'),
+  logos_ruby: require('./logos/ruby.png'),
+  logos_scala: require('./logos/scala.png'),
+  npm: require('./npm.png'),
+  server: require('./server.png'),
+  server2: require('./server2.png'),
+};
+export default images;
